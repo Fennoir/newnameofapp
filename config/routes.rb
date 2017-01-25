339769409a0b8e19440375f1ab54ced6eb8ca6a5
Steve Fenner <s.fenner@peachtechnologies.co.uk>
2017-01-25 12:39:38 +0000

    3.8 add description of change

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Nameofapp::Application.routes.draw do
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
+  get "static_pages/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Nameofapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to =>'static_pages#index'
 
   # See how all your routes lay out with "rake routes"
 
